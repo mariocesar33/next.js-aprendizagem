6cf54f37f0ec04932a9c57f0f5de58d0355f1a1d
Mário César Silva <mcgato33@hotmail.com>
2020-10-04 14:47:09 -0100

    servidor fake

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,42 @@
+import { GetServerSideProps } from 'next';
 import { Title  } from '../styles/pages/Home';
 
-export default function Home() {
+interface IProduct {
+  id: string;
+  title: string;
+}
+
+interface HomeProps {
+  recommendedProducts: IProduct[];
+}
+
+export default function Home({ recommendedProducts }: HomeProps) {
   return (
     <div>
-      <Title>ola Next</Title>
+      <section>
+        <Title>Produtos</Title>
+
+        <ul>
+          {recommendedProducts.map(recommendedProduct => {
+            return(
+              <li key={recommendedProduct.id}>
+                {recommendedProduct.title}
+              </li>
+            );
+          })}
+        </ul>
+      </section>
     </div>
   );
 };
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch('http://localhost:3333/recommended');
+  const recommendedProducts = await response.json();
+
+  return {
+    props: {
+      recommendedProducts
+    }
+  }
+}
