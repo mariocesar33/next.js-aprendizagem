e8a60334d52a1cbfdb9e44190a642323e9153477
Mário César Silva <mcgato33@hotmail.com>
2020-10-08 16:27:29 -0100

    laser lourdes para componentes

---
+++ src/pages/catalog/products/[slug].tsx
@@ -0,0 +1,27 @@
+import { useRouter } from 'next/router';
+import dynamic  from 'next/dynamic';
+import { useState } from 'react';
+
+const AddToCartModal = dynamic(
+  () => import('../../../components/AddToCardModal'),
+  { loading: () => <p>Carregando...</p> }
+);
+
+export default function product() {
+  const router = useRouter();
+  const [isAddToCartModalVisible, setIsAddToCartModalVisible] = useState(false);
+
+  function handleAddTocart() {
+    setIsAddToCartModalVisible(true)
+  }
+
+  return (
+    <div>
+      <h1>{router.query.slug}</h1>
+
+      <button onClick={handleAddTocart}>Add produto</button>
+
+      { isAddToCartModalVisible && <AddToCartModal /> }
+    </div>
+  );
+}
